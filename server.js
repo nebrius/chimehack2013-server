da96322a8b00d42b61145e28049466b0c92471e4
Bryan Hughes <bryan@theoreticalideations.com>
2013-12-07 02:23:27 -0800

    Implemented the backend

---
+++ server.js
@@ -0,0 +1,117 @@
+/*
+The MIT License (MIT)
+
+Copyright (c) 2013 Bryan Hughes, Nicole Jiang, Jaayden Halko , Bonnie Li
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+var fs = require('fs'),
+	path = require('path'),
+	express = require('express'),
+
+	app = express(),
+
+	PORT = 8080,
+	BATCH_DELAY = 200;
+
+function createEndpoint(name, autoIncrement) {
+	var counter = 0,
+		filePath = path.join(__dirname, 'data', name + 's.json'),
+		data = JSON.parse(fs.readFileSync(filePath)),
+		saving;
+
+	function save() {
+		if(!saving) {
+			saving = true;
+			setTimeout(function () {
+				fs.writeFileSync(filePath, JSON.stringify(data, false, '\t'));
+				saving = false;
+			}, BATCH_DELAY);
+		}
+	}
+
+	// GET /api/<name> -> Array<type>
+	app.get('/api/' + name, function (request, response) {
+		console.log('Getting the list of ' + name + 's');
+		response.send(200, JSON.stringify(data));
+	});
+
+	// GET /api/<name>/:id -> type
+	app.get('/api/' + name + '/:id', function (request, response) {
+		var requestId = request.params.id,
+			entry = data[requestId];
+		console.log('Getting the ' + name + ' with id ' + requestId);
+		if (!entry) {
+			console.error('Invalid request, ' + name + ' id "' + requestId + '" was not found');
+			response.send(400, 'Invalid request');
+			return;
+		}
+		response.send(200, JSON.stringify(entry));
+	});
+
+	// POST /api/students(/:id) type -> type
+	if (autoIncrement) {
+		app.put('/api/' + name, function (request, response) {
+			var info = request.body,
+				id = ++counter;
+			console.log('Creating new ' + name + ' ' + id);
+
+			// Save the data
+			info.id = id;
+			data.push(info);
+			save();
+
+			// Send the message
+			response.send(200, id);
+		});
+	}
+
+	// PUT /api/<name>/:id type -> type
+	app.put('/api/' + name + '/:id', function (request, response) {
+		var id = request.params.id,
+			info = request.body;
+		console.log((data[id] ? 'Updating' : 'Creating') + ' the ' + name + ' with id ' + id);
+
+		// Save the data
+		data[id] = info;
+		save();
+
+		// Send the response
+		response.send(200, info);
+	});
+}
+
+// Initialize express
+app.use(express.bodyParser());
+app.all('*', function(req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Content-Length');
+	next();
+});
+
+// Create the endpoints
+createEndpoint('student');
+createEndpoint('donor');
+createEndpoint('donation', true);
+
+// Start the app
+app.listen(PORT);
+console.log('Server started on port ' + PORT);
